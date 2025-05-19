8e9aceb8754eaa6d4a06a400822a6a15b3556de0
Juanco <juanffo874@gmail.com>
2025-05-18 21:29:57 -0600

    Crea la navegación entre las dos pagínas

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router"
+
 const LoginView = () => {
   return (
-    <div>LoginView</div>
+    <>
+      <div>LoginView</div>
+
+      <nav>
+        <Link to="/auth/register">
+          ¿No tienes cuenta? Regístrate
+        </Link>
+      </nav>
+    </>
   )
 }
 
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router"
+
 const RegisterView = () => {
   return (
-    <div>RegisterView</div>
+    <>
+      <div>RegisterView</div>
+
+      <nav>
+        <Link to="/auth/login">
+          ¿Ya tienes una cuenta? Inicia Sesión
+        </Link>
+      </nav>
+    </>
   )
 }
 
