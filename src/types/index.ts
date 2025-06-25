8b8ecbb32ed279a99ca4ba9135806e9c6b302849
Juanco <juanffo874@gmail.com>
2025-06-24 23:33:41 -0600

    Añade el formulario de registro y useForm

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,7 @@ export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
     password: string,
     password_confirmation: string
 }
+
+export type LoginForm = Pick<User, 'email'> & {
+    password: string
+}
