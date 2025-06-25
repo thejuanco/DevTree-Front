ada2ae663dfd9e78b3f9e4028a2c1ddebd14fd45
Juanco <juanffo874@gmail.com>
2025-06-24 23:25:59 -0600

    Crea un Cliente axios para realizar peticiones a la misma URL

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router"
 import { useForm } from "react-hook-form"
-import axios, { isAxiosError } from "axios"
+import { isAxiosError } from "axios"
 import { toast } from "sonner"
 import ErrorMessage from "../components/ErrorMessage"
+import api from "../config/axios"
 import type { RegisterForm } from "../types"
 
 const RegisterView = () => {
@@ -19,7 +20,7 @@ const RegisterView = () => {
 
   const handleRegister = async (formData : RegisterForm) => {
     try {
-      const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData)
+      const {data} = await api.post(`/auth/register`, formData)
       toast.success(data.message)
       //Reiniciar el formulario
       reset(initialValues)
