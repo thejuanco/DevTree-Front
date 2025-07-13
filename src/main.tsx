6e800078d740e1f89c3374be10b29523c875461a
Juanco <juanffo874@gmail.com>
2025-07-12 22:56:57 -0600

    Instalacion y configuracion de React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 // import App from './App.tsx'
 import Router from './router.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
   </StrictMode>,
 )
