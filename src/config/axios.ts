516e2dc4974868e6098238d3b8c60e487a23c60c
Juanco <juanffo874@gmail.com>
2025-07-15 19:47:41 -0600

    Añande al JWT en un interceptor de axios

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,4 +4,14 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
+//Agrega un interceptor a la petición
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('AUTH_TOKEN')
+
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
 export default api
