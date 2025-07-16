26d6dcb03d62592062084a57de71b5cd6e995df3
Juanco <juanffo874@gmail.com>
2025-07-15 20:12:29 -0600

    Obteniendo información del usuario y mostrandola en el componente

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,10 +1,11 @@
 import { isAxiosError } from "axios"
 import api from "../config/axios"
+import type { User } from "../types"
 
 export async function getUser() {
     const token = localStorage.getItem('AUTH_TOKEN')
     try {
-      const {data} = await api(`/user`, {
+      const {data} = await api<User>(`/user`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
