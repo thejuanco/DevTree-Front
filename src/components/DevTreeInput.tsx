495c126027008ab7a651efea83319250833084a0
Juanco <juanffo874@gmail.com>
2025-09-03 17:32:05 -0600

    Habilita o deshabilita el enlace

--- src/components/DevTreeInput.tsx
+++ src/components/DevTreeInput.tsx
@@ -5,9 +5,10 @@ import type { DevTreeLink } from "../types"
 type DevTreeInputProps = {
     item: DevTreeLink
     handleUrlChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    handleEnableLink: (socialNetwork: string) => void
 }
 
-export default function DevTreeInput({ item, handleUrlChange }: DevTreeInputProps) {
+export default function DevTreeInput({ item, handleUrlChange, handleEnableLink}: DevTreeInputProps) {
     return (
         <div className="bg-white shadow-sm flex p-5 items-center gap-3 rounded-md">
             <div
@@ -24,7 +25,7 @@ export default function DevTreeInput({ item, handleUrlChange }: DevTreeInputProp
 
             <Switch
                 checked={item.enabled}
-                onChange={() => { }}
+                onChange={() => handleEnableLink(item.name)}
                 className={classNames(
                     item.enabled ? 'bg-blue-500' : 'bg-gray-200',
                     'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -8,7 +8,11 @@ export default function LinkTreeView () {
 
   const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
     const updatedLinks = devTreeLinks.map(link => link.name == e.target.name ? { ...link, url: e.target.value } : link)
-    console.log(updatedLinks)
+    setDevTreeLinks(updatedLinks)
+  }
+
+  const handleEnableLink = (socialNetwork : string) => {
+    const updatedLinks = devTreeLinks.map(link => link.name === socialNetwork ? {...link, enabled: !link.enabled} : link)
     setDevTreeLinks(updatedLinks)
   }
 
@@ -20,6 +24,7 @@ export default function LinkTreeView () {
             key={item.name}
             item={item}
             handleUrlChange={handleUrlChange}
+            handleEnableLink={handleEnableLink}
           />
         ))}
       </div>
