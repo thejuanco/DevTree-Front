f5f830d68efcf691f7608c10a84c2e8f422a2398
Juanco <juanffo874@gmail.com>
2025-09-03 17:39:48 -0600

    Valida si la url es valida

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,13 @@
+//Clases proporcionadas por la documentacion de hadlessui
 export function classNames(...classes : string[]) {
     return classes.filter(Boolean).join(' ')
+}
+
+export function isValidUrl(url: string){
+    try {
+        new URL(url)
+        return true
+    } catch (error) {
+        return false        
+    }
 }
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { social } from "../data/social"
 import DevTreeInput from "../components/DevTreeInput"
+import { isValidUrl } from "../utils"
+import { toast } from "sonner"
 
 export default function LinkTreeView () {
 
@@ -12,7 +14,16 @@ export default function LinkTreeView () {
   }
 
   const handleEnableLink = (socialNetwork : string) => {
-    const updatedLinks = devTreeLinks.map(link => link.name === socialNetwork ? {...link, enabled: !link.enabled} : link)
+    const updatedLinks = devTreeLinks.map(link => {
+      if(link.name === socialNetwork){
+        if(isValidUrl(link.url)){
+          return {...link, enabled: !link.enabled} 
+        }else {
+          toast.error("Url no válida");
+        }
+      }
+      return link
+    })
     setDevTreeLinks(updatedLinks)
   }
 
