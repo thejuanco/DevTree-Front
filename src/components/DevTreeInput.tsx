0e04f281ae0821b31f7d673cc7439237fb67fe48
Juanco <juanffo874@gmail.com>
2025-09-03 17:14:22 -0600

    Identifica la red social en la que se escribe

--- src/components/DevTreeInput.tsx
+++ src/components/DevTreeInput.tsx
@@ -4,9 +4,10 @@ import type { DevTreeLink } from "../types"
 
 type DevTreeInputProps = {
     item: DevTreeLink
+    handleUrlChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function DevTreeInput({ item }: DevTreeInputProps) {
+export default function DevTreeInput({ item, handleUrlChange }: DevTreeInputProps) {
     return (
         <div className="bg-white shadow-sm flex p-5 items-center gap-3 rounded-md">
             <div
@@ -16,6 +17,9 @@ export default function DevTreeInput({ item }: DevTreeInputProps) {
             <input
                 type="text"
                 className="flex-1 border border-gray-100 rounded-lg"
+                value={item.url}
+                onChange={handleUrlChange}
+                name={item.name}
             />
 
             <Switch
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -6,6 +6,10 @@ export default function LinkTreeView () {
 
   const [devTreeLinks, setDevTreeLinks] = useState(social)
 
+  const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+  }
+
   return (
     <>
       <div className="space-y-5">
@@ -13,6 +17,7 @@ export default function LinkTreeView () {
           <DevTreeInput
             key={item.name}
             item={item}
+            handleUrlChange={handleUrlChange}
           />
         ))}
       </div>
