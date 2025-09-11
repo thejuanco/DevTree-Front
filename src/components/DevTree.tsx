3f097c7b13898d7b3dff19ddc79c14d698f61d33
Juanco <juanffo874@gmail.com>
2025-09-10 22:00:08 -0600

    Añade funcionalidad de Drop

--- src/components/DevTree.tsx
+++ src/components/DevTree.tsx
@@ -18,8 +18,15 @@ export default function DevTree({ data }: DevTreeProps) {
         setEnabledLinks(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
     }, [data])
 
-    const handleDragEnd = () => {
-        console.log('Hola')
+    const handleDragEnd = (e : DragEndEvent) => {
+        const {over, active} = e
+
+        if(over && over.id){
+            const prevIndex = enabledLinks.findIndex(link => link.id === active.id)
+            const newIndex = enabledLinks.findIndex(link => link.id === over.id)
+            const order = arrayMove(enabledLinks, prevIndex, newIndex)
+            setEnabledLinks(order)
+        }
     }
 
     return (
