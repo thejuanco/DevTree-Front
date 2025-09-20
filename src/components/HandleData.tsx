e7624aa52e699810c66ea29afe8862c9a48132a9
Juanco <juanffo874@gmail.com>
2025-09-19 23:02:41 -0600

    Renderiza los datos del usuario

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -6,6 +6,11 @@ type HandleDataProps = {
 
 export default function HandleData({data} : HandleDataProps) {
   return (
-    <div>HandleData</div>
+    <div className="space-y-6 text-white">
+      <p className="text-5xl text-center font-black">{data.handle}</p>
+      {data.image && <img src={data.image} className="max-w-[250px] mx-auto"/>}
+
+      <p className="text-lg text-center font-bold">{data.description}</p>
+    </div>
   )
 }
