4ac2ccca6667ea77b94d79307ac8d635181a1ef9
Juanco <juanffo874@gmail.com>
2025-09-27 16:50:59 -0600

    Instala y genera los slugs

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form"
+import slugify from "react-slugify"
 import ErrorMessage from "./ErrorMessage"
 
 export default function SearchForm() {
@@ -16,7 +17,8 @@ export default function SearchForm() {
     const handle = watch('handle')
 
     const handleSearch = () => {
-        console.log(handle)
+        const slug = slugify(handle, {delimiter: "_"})
+        console.log(slug)
     }
 
     return (
